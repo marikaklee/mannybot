d794556f4adff140282b42431e9e8b9ab18bd1f9
marikaklee <marikalee15@gmail.com>
2017-02-24 01:42:20 -0800

    add log

--- mannybot.py
+++ mannybot.py
@@ -9,9 +9,17 @@ from threading import Timer
 import urllib2
 from twitter import Twitter, TwitterError, TwitterHTTPError
 import sched
+import logging
 
 TWITTER_ACCOUNT = 'marikalee15'
 TWEET_TIME = 1; #4PM
+
+logger = logging.getLogger('tweetbott')
+hdlr = logging.FileHandler('logs.log')
+formatter = logging.Formatter('%(asctime)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
+logger.setLevel(logging.WARNING)
 
 #---------Read from Google Spreadsheet file
 # import gspread
@@ -33,11 +41,13 @@ TWEET_TIME = 1; #4PM
 
 my_bot = TwitterBot()
 
+logger.error("testing")
 def executeTweets():
 	#lines = open('/Users/marika.lee/Dropbox/mannyBot/%s' % TWITTER_ACCOUNT + '/tweets.txt').readLines()
 	lines = open('tweets.txt').readlines()
 
 	if (len(lines) < 1):
+		logger.error("!!!NO MORE TWEETS TO TWEET!!!")
 		return
 
 	tweet = lines[0]
@@ -45,22 +55,23 @@ def executeTweets():
 	open('done.txt', 'w').writelines(tweet)
 	open('tweets.txt', 'w').writelines(lines[1:len(lines)])
 
-	print "[{:%b %d | %H:%M}".format(datetime.today()) + "] Tweeting...", tweet,
+	logger.error("[{:%b %d | %H:%M}".format(datetime.today()) + "] Tweeting...", tweet)
 
 	try:
 		if datetime.now().hour == TWEET_TIME:
 			my_bot.send_tweet(tweet)
-			print "!!!SUCCESSFULL TWEET!!!"
+			print (tweet)
+			logger.error("!!!SUCCESSFULL TWEET!!!")
 
 	except TwitterError as err:
    		if err.e.code == 404:
-   			print "ERROR 404 Page not found"
+   			logger.error("ERROR 404 Page not found")
    		elif err.e.code == 403:
-   			print "ERROR 403 Status is duplicate"
+   			logger.error("ERROR 403 Status is duplicate")
 		else:
-			print "ERROR ", err.e.code
+			logger.error("ERROR ", err.e.code)
 			
-schedule.every(.1).minutes.do(executeTweets)
+schedule.every(3).minutes.do(executeTweets)
 #schedule.every().hour.do(job)
 #schedule.every().day.at(str(TWEET_TIME)).do(executeTweets)
 
@@ -68,5 +79,5 @@ while 1:
     schedule.run_pending()
     time.sleep(1)
 
-
+#notifiy when there are no tweets to tweet 
 
